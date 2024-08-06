388afa3eca65570c3886b1c3aba517af939ff55b
taka <gluestick829@gmail.com>
2024-08-06 19:01:55 +0900

    Add:注文完了画面の表示処理を追加 MenuThanksActivity.ktのonCreate内にIntentSampleからコピー

--- app/src/main/java/com/example/menusample/MenuThanksActivity.kt
+++ app/src/main/java/com/example/menusample/MenuThanksActivity.kt
@@ -1,6 +1,8 @@
 package com.example.menusample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,17 @@ class MenuThanksActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val menuName = intent.getStringExtra("menuName")
+        val menuPrice = intent.getStringExtra("menuPrice")
+
+        val tvMenuName = findViewById<TextView>(R.id.tvMenuName)
+        val tvMenuPrice = findViewById<TextView>(R.id.tvMenuPrice)
+        tvMenuName.text = menuName
+        tvMenuPrice.text = menuPrice
+    }
+
+    fun onBackButtonClick(view: View) {
+        finish()
     }
 }
